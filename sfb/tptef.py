cf473538fd86dbb10418ac4e284c0296553ca941
jSm449g4d <47020182+jSm449g4d@users.noreply.github.com>
2020-03-10 03:13:48 +0900

    Add files via upload

--- sfb/tptef.py
+++ sfb/tptef.py
@@ -42,8 +42,10 @@ def show(req):
         if "clear" in req.form and secure_filename(req.form["clear"])=="True":
             doc_ref.where("trip", "==", hashlib.sha256(passwd.encode('utf-8')).hexdigest()).delete()
     #show chat thread
-        for order in doc_ref.get():
-            1
+        orders=doc_ref.get().to_dict()
+        
+#        for order in orders:
+#            1
 #            orders+="<tr><td>"+order.to_dict()["user"]+"</td>"
 #            orders+="<td>"+order.to_dict()["content"]+"</td>"
 #            orders+="<td style=\"font-size: 12px;\">"+(order.to_dict()["trip"])[:16]+"<br>"+(order.to_dict()["trip"])[16:32]+\

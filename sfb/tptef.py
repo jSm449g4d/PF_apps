fd45b946a1e1b502949ca8b663f7e19fc9e066f2
jSm449g4d <47020182+jSm449g4d@users.noreply.github.com>
2020-03-09 23:33:11 +0900

    Add files via upload

--- sfb/tptef.py
+++ sfb/tptef.py
@@ -28,7 +28,8 @@ def show(req):
         if 'pass' in req.form:
             passwd=secure_filename(req.form['pass'])
             
-        doc_ref = wsgi_util.db.document("tptef/A")#collection(room).
+        doc_ref = wsgi_util.db.document("tptef/"+room)#collection(room).
+        doc_ref= doc_ref.document("B") 
         if "remark" in req.form and secure_filename(req.form["remark"])=="True":
             doc_ref.set({"user": user,"content": content,"trip":hashlib.sha256(passwd.encode('utf-8')).hexdigest(),
                         "date":datetime.now(pytz.UTC).strftime("%Y/%m/%d %H:%M:%S %f (UTC)")})

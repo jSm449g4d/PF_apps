faaa74a368777b9df07ed78ca082e6369e41e415
jSm449g4d <47020182+jSm449g4d@users.noreply.github.com>
2020-03-11 06:46:38 +0900

    Add files via upload

--- sfb/static/src/tptef.js
+++ sfb/static/src/tptef.js
@@ -1,6 +1,3 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
 class App extends React.Component {
     constructor(props) {
         super(props);
@@ -20,14 +17,7 @@ class App extends React.Component {
     }
 
     render() {
-        return (
-            <form onSubmit={this.handleSubmit}>
-                <label>
-                    Name:
-                        <input type="text" value={this.state.value} onChange={this.handleChange} />
-                </label>
-                <input type="submit" value="Submit" />
-            </form>
+        return (abc
         );
     }
 }

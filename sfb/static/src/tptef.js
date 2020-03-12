e8a10e8f833385bbad55abbe9037bbf85a9f29d1
jSm449g4d <47020182+jSm449g4d@users.noreply.github.com>
2020-03-13 06:53:00 +0900

    Add files via upload

---
+++ sfb/static/src/tptef.js
@@ -0,0 +1,20 @@
+class Tstreact extends React.Component {
+    handleSubmit(event) { alert('A name was submitted: ' + this.state.value); event.preventDefault(); }
+    constructor(props) {
+        super(props); this.state = { value: '' };
+        this.handleChange = this.handleChange.bind(this); this.handleSubmit =
+            this.handleSubmit.bind(this);
+    }
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <label>
+                    Name:
+            <input type="text" value={this.state.value} onChange={this.handleChange} />
+                </label>
+                <input type="submit" value="Submit" />
+            </form>
+        );
+    }
+}; ReactDOM.render(
+    <Tstreact />, document.getElementById('Mail'));

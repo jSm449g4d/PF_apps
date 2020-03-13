7089cebe5acb0a33a4154b669605bf35f5654431
jSm449g4d <47020182+jSm449g4d@users.noreply.github.com>
2020-03-14 08:05:40 +0900

    Add files via upload

--- sfb/static/src/tptef.js
+++ sfb/static/src/tptef.js
@@ -1,36 +1,30 @@
 //plz use react-bootstrap and Firebase
 
-
 class Tstreact extends React.Component {
-
-    handleSubmit(event) {
-        this.setState({
-            showPopup: !this.state.showPopup
-        });
-    };
 
     constructor(props) {
         super(props);
-        this.state = { value: 'SUBmit', showPopup: false };
+        this.state = { value: 'SUBmit' };
     };
     render() {
-        var user = firebase.auth().currentUser;
-        if (user) {
-            return (
-                <div>
-                    <input type="button" value="logout" class="btn btn-success" onClick={auth_logout} />
-                </div>
-            )
-        } else {
-            return (
-                <div>
-                    <input type="button" value="auth_google" class="btn btn-success" onClick={auth_google} />
-                    <input type="button" value="auth_guest" class="btn btn-success" onClick={auth_guest} />
-                    <input type="button" value="auth_mail" class="btn btn-success" onClick={auth_mail} />
-                    <input type="button" value="auth_mail_add" class="btn btn-success" onClick={auth_mail_add} />
-                </div>
-            )
-        }
+        firebase.auth().onAuthStateChanged(function (user) {
+            if (user) {
+                return (
+                    <div>
+                        <input type="button" value="logout" class="btn btn-success" onClick={auth_logout} />
+                    </div>
+                )
+            } else {
+                return (
+                    <div>
+                        <input type="button" value="auth_google" class="btn btn-success" onClick={auth_google} />
+                        <input type="button" value="auth_guest" class="btn btn-success" onClick={auth_guest} />
+                        <input type="button" value="auth_mail" class="btn btn-success" onClick={auth_mail} />
+                        <input type="button" value="auth_mail_add" class="btn btn-success" onClick={auth_mail_add} />
+                    </div>
+                )
+            }
+        });
     }
 };
 

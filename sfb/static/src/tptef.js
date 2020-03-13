04951e4d120f0372ea5149e82c4e31468f449bc7
jSm449g4d <47020182+jSm449g4d@users.noreply.github.com>
2020-03-14 06:08:56 +0900

    Add files via upload

--- sfb/static/src/tptef.js
+++ sfb/static/src/tptef.js
@@ -35,17 +35,14 @@ class Tstreact extends React.Component {
         if (user) {
             return (
                 <div>
-                <input type="button" value="auth_google" class="btn btn-success" onclick="auth_google();" />
-                <input type="button" value="auth_guest" class="btn btn-success" onclick="auth_guest();" />
-                <input type="button" value="auth_mail" class="btn btn-success" onclick="auth_mail();" />
-                <input type="button" value="auth_mail_add" class="btn btn-success" onclick="auth_mail_add();" />
+                    bbbbb
                 </div>
                 )
 
         } else {
             return (
                 <div>
-                <input type="button" value="logout" class="btn btn-success" onclick="auth_logout();" />
+                    aaaa
                 </div>
                 )
         }

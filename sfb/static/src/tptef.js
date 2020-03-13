11a90503858165a24bd5159a61a041e703d255c2
jSm449g4d <47020182+jSm449g4d@users.noreply.github.com>
2020-03-14 03:43:53 +0900

    Add files via upload

--- sfb/static/src/tptef.js
+++ sfb/static/src/tptef.js
@@ -2,8 +2,14 @@
 
 function Popmaillogin() {
     return (
-    <div>
-    <h1>ASASSASAS</h1></div>
+        <div>
+            <h1>ASASSASAS</h1>
+
+            <div className='popup' style="z-index:1;display:block;position:fixed;top:0;left:0;width:100%;height:100%;background-color:rgba(51,51,51,0.6);margin-top:0px;">
+                <div className='popup_inner' style="top:10%;left:25%;width:50%;height:auto;"> aaacaa</div>
+            </div>
+
+        </div>
     )
 }
 

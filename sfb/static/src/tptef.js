09b25aa6e136bf5c342910b6817f1b8cc85bfce0
jSm449g4d <47020182+jSm449g4d@users.noreply.github.com>
2020-03-13 21:01:43 +0900

    Add files via upload

--- sfb/static/src/tptef.js
+++ sfb/static/src/tptef.js
@@ -3,9 +3,10 @@ class Tstreact extends React.Component {
     
     handleSubmit(event) { alert('A name was submitted: ' + this.state.value); event.preventDefault(); };
     constructor(props) {
-        /*super(props); this.state = { value: '' };
-        this.handleChange = this.handleChange.bind(this); this.handleSubmit =
-            this.handleSubmit.bind(this);*/
+        super(props);
+        this.state = { value: '' };
+        /*this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);*/
     };
     render() {
         return (

a8af97dc68b0ecca836c9a0d4ff58b3ca983fdfb
jSm449g4d <47020182+jSm449g4d@users.noreply.github.com>
2020-03-14 05:00:20 +0900

    Add files via upload

--- sfb/static/src/tptef.js
+++ sfb/static/src/tptef.js
@@ -9,12 +9,11 @@ const customStyles = {
 function Popmaillogin() {
     return (
         <div>
-            <h1>ASASSASAS</h1>
-
-            <div style={customStyles}>
-                <div> aaacaa</div>
-            </div>
-
+        <input type="button" value="auth_google" class="btn btn-success" onclick="auth_google();" />
+        <input type="button" value="auth_guest" class="btn btn-success" onclick="auth_guest();" />
+        <input type="button" value="auth_mail" class="btn btn-success" onclick="auth_mail();" />
+        <input type="button" value="auth_mail_add" class="btn btn-success" onclick="auth_mail_add();" />
+        <input type="button" value="logout" class="btn btn-success" onclick="auth_logout();" />
         </div>
     )
 }
@@ -32,13 +31,32 @@ class Tstreact extends React.Component {
         this.state = { value: 'SUBmit', showPopup: false };
     };
     render() {
+        user=firebase.auth().currentUser
+        if (user) {
+            return (
+                <div>
+                <input type="button" value="auth_google" class="btn btn-success" onclick="auth_google();" />
+                <input type="button" value="auth_guest" class="btn btn-success" onclick="auth_guest();" />
+                <input type="button" value="auth_mail" class="btn btn-success" onclick="auth_mail();" />
+                <input type="button" value="auth_mail_add" class="btn btn-success" onclick="auth_mail_add();" />
+                </div>
+                )
+
+        } else {
+            return (
+                <div>
+                <input type="button" value="logout" class="btn btn-success" onclick="auth_logout();" />
+                </div>
+                )
+        }
+
+
+        
         return (<p>
             <label>
                 Name:
                 </label>
             <input type="button" class="btn btn-success mx-1" value={this.state.value} onClick={this.handleSubmit.bind(this)} />
-
-
             {this.state.showPopup ?
                 <Popmaillogin text='Close Me' closePopup={this.handleSubmit.bind(this)} />
                 : null}

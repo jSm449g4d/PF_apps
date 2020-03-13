ac8bae7233abc9c838dfcad576f4affa7a9cc6ec
jSm449g4d <47020182+jSm449g4d@users.noreply.github.com>
2020-03-14 07:45:50 +0900

    Add files via upload

--- sfb/static/src/tptef.js
+++ sfb/static/src/tptef.js
@@ -70,16 +70,16 @@ class Tstreact extends React.Component {
         if (user) {
             return (
                 <div>
-                    <input type="button" value="logout" class="btn btn-success" onclick={auth_logout} />
+                    <input type="button" value="logout" class="btn btn-success" onClick={auth_logout} />
                 </div>
             )
         } else {
             return (
                 <div>
-                    <input type="button" value="auth_google" class="btn btn-success" onclick={auth_google} />
-                    <input type="button" value="auth_guest" class="btn btn-success" onclick={auth_guest} />
-                    <input type="button" value="auth_mail" class="btn btn-success" onclick={auth_mail} />
-                    <input type="button" value="auth_mail_add" class="btn btn-success" onclick={auth_mail_add} />
+                    <input type="button" value="auth_google" class="btn btn-success" onClick={auth_google} />
+                    <input type="button" value="auth_guest" class="btn btn-success" onClick={auth_guest} />
+                    <input type="button" value="auth_mail" class="btn btn-success" onClick={auth_mail} />
+                    <input type="button" value="auth_mail_add" class="btn btn-success" onClick={auth_mail_add} />
                 </div>
             )
         }

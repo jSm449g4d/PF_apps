7e939b77219bf4d6d17d229492d6a28cd8e9fc85
jSm449g4d <47020182+jSm449g4d@users.noreply.github.com>
2020-03-15 01:51:52 +0900

    Add files via upload

---
+++ sfb/sfb/wsgi.py
@@ -0,0 +1,41 @@
+# coding: utf-8
+import sys
+import os
+import flask
+from flask import  redirect,request,render_template_string,render_template
+from werkzeug.utils import secure_filename
+import importlib
+import random
+
+#Flask_Startup
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+os.chdir(os.path.join("./",os.path.dirname(__file__)))
+app = flask.Flask(__name__)
+wsgi_util=importlib.import_module("wsgi_util")
+#prevent uploading too large file
+app.config['MAX_CONTENT_LENGTH'] = 100000000
+
+@app.route("/")
+def indexpage_show():
+    wsgi_util.access_counter+=1
+    return wsgi_util.render_template_2("index.html",
+    STATUS_TABLE=wsgi_util.status_table,
+    access_counter=str(wsgi_util.access_counter)
+    )
+
+@app.route("/<name>.html")
+def html_show(name):
+    try :return wsgi_util.render_template_2('./'+name+'.html')
+    except:return redirect('./'),404
+
+@app.route("/<name>.py",methods=['GET', 'POST'])
+def py_show(name):
+    try :return importlib.import_module(name).show(request)
+    except Exception as e:
+        return wsgi_util.render_template_2("error.html",
+        form_error_code="500",form_error_text=str(e)),500
+
+application=app
+
+if __name__ == "__main__":
+    app.run()
---
+++ sfb/sfb/wsgi_util.py
@@ -0,0 +1,30 @@
+# coding: utf-8
+import sys
+import os
+import flask
+import json
+from flask import  render_template_string
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+def render_template_2(dir,**kwargs):
+    html=""
+    with open(os.path.join("./templates/",dir),"r",encoding="utf-8") as f:
+        html=f.read()
+        for kw,arg in kwargs.items():
+            html=html.replace("{{"+kw+"}}",arg)
+    return render_template_string(html)
+
+#AP_setting_management
+access_counter=0;status_table="";config_dict={}
+cred = firebase_admin.credentials.Certificate("FirebaseAdmin_Key.json")
+firebase_admin.initialize_app(cred)
+with open("config.json","r",encoding="utf-8") as fp:config_dict.update(json.load(fp))
+db = firestore.client()
+def add_status_table(title="",data="",color="navy"):
+    global status_table
+    status_table+="<tr><td style=\"color:"+color+";\">"+title+"</td><td style=\"color:"+color+";\">"+data+"</td></tr>"
+def Resource_Reload():
+    add_status_table("Python",sys.version,color="#555000")
+    add_status_table("Flask",flask.__version__,color="#555000")

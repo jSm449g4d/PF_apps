822c7b40199cb77519e6ee132eaa6fccea513b0c
jSm449g4d <47020182+jSm449g4d@users.noreply.github.com>
2020-03-19 16:24:48 +0900

    Add files via upload

--- sfb/static/src/account.js
+++ sfb/static/src/account.js
@@ -1,43 +1,37 @@
 //plz use react-bootstrap and Firebase
 
 class Account_tag extends React.Component {
-    create_account() {
-        auth.createUserWithEmailAndPassword(this.state.mail_addr, this.state.mail_pass).catch((error) =>
-            {alert("error_code:" + error.code + "\nerror_message:" + error.message)}
+    signup() {
+        auth.createUserWithEmailAndPassword(this.state.mail_addr, this.state.mail_pass).catch((error) => { alert("error_code:" + error.code + "\nerror_message:" + error.message) }
         )
     }
     signin() {
-        auth.signInWithEmailAndPassword(this.state.mail_addr, this.state.mail_pass).catch((error) =>
-            {alert("error_code:" + error.code + "\nerror_message:" + error.message)}
+        auth.signInWithEmailAndPassword(this.state.mail_addr, this.state.mail_pass).catch((error) => { alert("error_code:" + error.code + "\nerror_message:" + error.message) }
         )
     }
     pass_reset() {
         auth.sendPasswordResetEmail(this.state.mail_addr).then(() => {
             alert("SEND_EMAIL!")
-        }).catch((error) => {alert("error_code:" + error.code + "\nerror_message:" + error.message)}
+        }).catch((error) => { alert("error_code:" + error.code + "\nerror_message:" + error.message) }
         );
     }
     account_delete() {
         auth.currentUser.delete().then(() => {
             alert("ACCOUNT_DELETED!")
-        }).catch((error) =>
-            {alert("error_code:" + error.code + "\nerror_message:" + error.message)}
+        }).catch((error) => { alert("error_code:" + error.code + "\nerror_message:" + error.message) }
         );
     }
     signin_easy() {
         auth.signInWithEmailAndPassword("a@b.com", "asdfgh").catch(() => {
-            auth.createUserWithEmailAndPassword("a@b.com", "asdfgh").catch((error) =>
-                {alert("error_code:" + error.code + "\nerror_message:" + error.message)}
+            auth.createUserWithEmailAndPassword("a@b.com", "asdfgh").catch((error) => { alert("error_code:" + error.code + "\nerror_message:" + error.message) }
             )
         })
     }
     google_login() {
-        auth.signInWithPopup(new firebase.auth.GoogleAuthProvider()).then().catch((error) =>
-            {alert("error_code:" + error.code + "\nerror_message:" + error.message);})
+        auth.signInWithPopup(new firebase.auth.GoogleAuthProvider()).then().catch((error) => { alert("error_code:" + error.code + "\nerror_message:" + error.message); })
     }
     handleChange(e) {
-        let name = e.target.name;
-        this.setState({ [name]: e.target.value })
+        this.setState({ [e.target.name]: e.target.value })
     }
 
     constructor(props) {
@@ -49,10 +43,39 @@ class Account_tag extends React.Component {
                 if (this.state.uid != auth.currentUser.uid) this.setState({ uid: auth.currentUser.uid })
             }
             else {
-                if (this.state.uid !="") this.setState({ uid: "" });
+                if (this.state.uid != "") this.setState({ uid: "" });
             }
         }, 100)
     };
+
+    accountmodal_render(title, func) {
+        let modal_id = "mygape_modal_" + title;
+        let modal_id_s = "#" + modal_id;
+        func = func.bind(this)
+        return (
+            <div>
+                <button type="button" class="btn btn-primary btn-sm mx-1" data-toggle="modal" data-target={modal_id_s}>{title}</button>
+                <div class="modal fade" id={modal_id} role="dialog" aria-hidden="true">
+                    <div class="modal-dialog modal-lg" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title">{title}</h5>
+                            </div>
+                            <div class="modal-body row">
+                                <input type="text" name="mail_addr" class="form-control col-6" placeholder="mail_address" onChange={this.handleChange} />
+                                <input type="text" name="mail_pass" class="form-control col-6" placeholder="set_password" onChange={this.handleChange} />
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-primary" data-dismiss="modal" onClick={func}>Submit</button>
+                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
     render() {
         return (
             <div class=""><nav class="navbar navbar-expand-lg navbar-light bg-light">
@@ -71,8 +94,8 @@ class Account_tag extends React.Component {
                                     <div class="modal-header">
                                         <h5 class="modal-title">Config</h5>
                                     </div>
-                                    <div class="modal-body">
-                                        <input type="text" name="mail_addr" class="form-control m-1" placeholder="send mail for password_reset" onChange={this.handleChange} />
+                                    <div class="modal-body row">
+                                        <input type="text" name="mail_addr" class="form-control col-12" placeholder="send mail for password_reset" onChange={this.handleChange} />
                                     </div>
                                     <div class="modal-footer">
                                         <button type="button" class="btn btn-sm btn-warning" data-dismiss="modal" onClick={this.pass_reset}>password_reset</button>
@@ -86,49 +109,15 @@ class Account_tag extends React.Component {
                     :
                     <div class="navber-brand">
                         <div class="navbar-right form-inline">サービスを利用するには、ログインしてください
-                            <input type="button" value="Googleでログイン" class="btn btn-success mx-1 btn-sm" onClick={this.google_login} />
-                            <button type="button" class="btn btn-success mx-1 btn-sm" data-toggle="modal" data-target="#account_modal_create_acc">Create_account</button>
-                            <button type="button" class="btn btn-success mx-1 btn-sm" data-toggle="modal" data-target="#account_modal_signin">Sign_in</button>
+                                <input type="button" value="Googleでログイン" class="btn btn-success mx-1 btn-sm" onClick={this.google_login} />
+                            {this.accountmodal_render("Sign_in", this.signin)}
+                            {this.accountmodal_render("Sign_up", this.signup)}
                             <button type="button" class="btn btn-warning mx-1 btn-sm" onClick={this.signin_easy}>Easy_login</button>
-                            <div class="modal fade" id="account_modal_signin" role="dialog" aria-hidden="true">
-                                <div class="modal-dialog" role="document">
-                                    <div class="modal-content">
-                                        <div class="modal-header">
-                                            <h5 class="modal-title">Sign in</h5>
-                                        </div>
-                                        <div class="modal-body">
-                                            <input type="text" name="mail_addr" class="form-control m-1" placeholder="mail_address" onChange={this.handleChange} />
-                                            <input type="text" name="mail_pass" class="form-control m-1" placeholder="set_password" onChange={this.handleChange} />
-                                        </div>
-                                        <div class="modal-footer">
-                                            <button type="button" class="btn btn-primary" data-dismiss="modal" onClick={this.signin}>Submit</button>
-                                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="modal fade" id="account_modal_create_acc" role="dialog" aria-hidden="true">
-                            <div class="modal-dialog" role="document">
-                                <div class="modal-content">
-                                    <div class="modal-header">
-                                        <h5 class="modal-title">Create account</h5>
-                                    </div>
-                                    <div class="modal-body">
-                                        <input type="text" name="mail_addr" class="form-control m-1" placeholder="mail_address" onChange={this.handleChange} />
-                                        <input type="text" name="mail_pass" class="form-control m-1" placeholder="set_password" onChange={this.handleChange} />
-                                    </div>
-                                    <div class="modal-footer">
-                                        <button type="button" class="btn btn-primary" data-dismiss="modal" onClick={this.create_account}>Submit</button>
-                                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                                    </div>
-                                </div>
-                            </div>
                         </div>
                     </div>
                 }
             </nav></div>
-            
+
         );
     };
 };
--- sfb/static/src/mypage.js
+++ sfb/static/src/mypage.js
@@ -2,19 +2,29 @@
 
 class Mypage_tag extends React.Component {
     load_profile() {
-        if (this.state.uid != "") {
-            var docRef = db.collection("mypage").doc(this.state.uid);
-            docRef.get().then((doc) => {
-                var profile = JSON.stringify(this.state); profile = JSON.parse(profile); delete profile["uid"];
-                if (!doc.exists) {
-                    docRef.set(profile);
-                    this.setState(profile);
-                }
-                else {
-                    this.setState(doc.data());
-                }
-            });
-        }
+        if (this.state.uid == "") return;
+        var docRef = db.collection("mypage").doc(this.state.uid);
+        docRef.get().then((doc) => {
+            let profile = JSON.parse(JSON.stringify(this.state)); delete profile["uid"];
+            if (!doc.exists) {
+                docRef.set(profile);
+                this.setState(profile);
+            }
+            else {
+                this.setState(doc.data());
+            }
+        });
+    }
+
+    update_profile() {
+        if (this.state.uid == "") return;
+        var docRef = db.collection("mypage").doc(this.state.uid);
+        docRef.get().then((doc) => {
+            let profile = JSON.parse(JSON.stringify(this.state)); delete profile["uid"];
+            if (doc.exists) {
+                docRef.set(profile);
+            }
+        });
     }
 
     componentDidMount() {
@@ -36,42 +46,38 @@ class Mypage_tag extends React.Component {
     constructor(props) {
         super(props);
         this.state = { uid: "" };
-
         setInterval(() => {
             if (auth.currentUser) {
                 if (this.state.uid != auth.currentUser.uid) this.setState({ uid: auth.currentUser.uid })
             }
             else {
-                if (this.state.uid !="") this.setState({ uid: "" });
+                if (this.state.uid != "") this.setState({ uid: "" });
             }
         }, 100)
     };
-    rec(as) { alert(as) }
 
-    changebutton_render(title,name, func) {
-        var modal_id = "mygape_modal_" + name;
-        var modal_id_s = "#"+modal_id;
-        var handlename = ""
+    changebutton_render(title, state_element) {
+        let modal_id = "mygape_modal_" + title;
+        let modal_id_s = "#" + modal_id;
         function changes(e) {
-            handlename = e.target.value;
-        }
-        function submit() {
-            func(handlename)
-        }
+            this.setState({ [state_element]: e.target.value })
+        }; changes = changes.bind(this);
+        this.update_profile = this.update_profile.bind(this)
         return (
             <div>
                 <button type="button" class="btn btn-outline-success btn-sm mx-1" data-toggle="modal" data-target={modal_id_s}>change</button>
                 <div class="modal fade" id={modal_id} role="dialog" aria-hidden="true">
-                    <div class="modal-dialog" role="document">
+                    <div class="modal-dialog modal-lg" role="document">
                         <div class="modal-content">
                             <div class="modal-header">
                                 <h5 class="modal-title">{title}</h5>
                             </div>
-                            <div class="modal-body">
-                                <input type="text" name="mail_addr" class="form-control m-1" placeholder="send mail for password_reset" onChange={changes} />
+                            <div class="modal-body row">
+                                    <textarea class="form-control col-12" rows="6" value={this.state[state_element]} onChange={changes} />
+
                             </div>
                             <div class="modal-footer">
-                                <button type="button" class="btn btn-sm btn-danger" data-dismiss="modal" onClick={submit}>SUBMIT</button>
+                                <button type="button" class="btn btn-sm btn-success" data-dismiss="modal" onClick={this.update_profile}>SUBMIT</button>
                                 <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
                             </div>
                         </div>
@@ -80,15 +86,15 @@ class Mypage_tag extends React.Component {
             </div>
         )
     }
-
+    //<input type="text" name="mail_addr" class="form-control m-1" value={this.state[state_element]} onChange={changes} />
     render() {
         return (
             <div>
                 {this.state.uid != "" ?
                     <div>
-                        <h3 class="form-inline">{this.state.nickname}{this.changebutton_render("nickname",this.state.nickname, this.rec)}</h3>
-                        <h5 class="form-inline">{this.state.pr}{this.changebutton_render("PR",this.state.pr, this.rec)}</h5>
-                        <h5 class="form-inline">{this.state.timestamp}{this.changebutton_render("Timestamp",this.state.timestamp, this.rec)}</h5>
+                        <h3 class="form-inline">{this.state.nickname}{this.changebutton_render("nickname", "nickname")}</h3>
+                        <h5 class="form-inline">{this.state.pr}{this.changebutton_render("PR", "pr")}</h5>
+                        <h5 class="form-inline">{this.state.timestamp}{this.changebutton_render("Timestamp", "timestamp")}</h5>
                     </div>
                     :
                     <h4>This application cant use without login</h4>

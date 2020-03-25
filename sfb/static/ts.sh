16d52bb6d2b14944aca55e9742fcbced39e7c9f2
jSm449g4d <47020182+jSm449g4d@users.noreply.github.com>
2020-03-25 13:49:50 +0900

    test

--- sfb/static/ts.sh
+++
@@ -1,6 +0,0 @@
-npm install typescript -g
-npm install --save react react-dom
-npm install --save-dev @types/react @types/react-dom
-npm install --save-dev typescript ts-loader source-map-loader
-npm install --save-dev webpack webpack-cli
-npx webpack

49f0ca366b166f08fb14406098fa2931d4272272
jSm449g4d <47020182+jSm449g4d@users.noreply.github.com>
2020-03-25 17:25:06 +0900

    bootstrap

--- sfb/static/ts/acc.tsx
+++ sfb/static/ts/acc.tsx
@@ -2,17 +2,51 @@
 
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import { Nav, NavItem, Navbar, Row, Col} from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
+//import { Nav, NavItem, Navbar, Row, Col } from "react-bootstrap";
+/*
 import * as firebase from 'firebase/app';
+import "firebase/auth";
 
-export class Acc extends React.Component{
-    render() {
-        return <h1>acc</h1>;
+firebase.initializeApp({
+    apiKey: "AIzaSyCWzFat3oUpn_4TtOpDCMhcOD2Qf4u1Mr4",
+    authDomain: "crack-atlas-251509.firebaseapp.com",
+    databaseURL: "https://crack-atlas-251509.firebaseio.com",
+    projectId: "crack-atlas-251509",
+    storageBucket: "crack-atlas-251509.appspot.com",
+    messagingSenderId: "646437940818",
+    appId: "1:646437940818:web:080ff48019a68c74d3b98b",
+    measurementId: "G-QLHKJ38SWW"
+})
+// Initialize Firebase
+firebase.analytics();
+var auth = firebase.auth();
+var db = firebase.firestore();
+var storage = firebase.storage();
+auth.onAuthStateChanged((user) => {
+    if (user) {
+        user.getIdToken(true).then((idToken) => {
+            document.getElementsByName("fbtoken")[0].setAttribute("value", idToken);
+        })
+    } else {
+        document.getElementsByName("fbtoken")[0].setAttribute("value", "");
     }
+})
+*/
+interface State {
+    uid: "", mail_addr: "", mail_pass: "";
+}
+
+export class Acc extends React.Component<{}, State> {
     
+    render() {
+        return <div className="bg-light p-2">acc</div>;
+    }
+
 }
 
 ReactDOM.render(
     <Acc />,
     document.getElementById("acc")
 );
+

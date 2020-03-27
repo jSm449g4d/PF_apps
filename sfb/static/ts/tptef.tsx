3fc63aba5351865b04f16fad6692944f2b456bf9
jSm449g4d <47020182+jSm449g4d@users.noreply.github.com>
2020-03-27 12:43:18 +0900

    a

--- sfb/static/ts/tptef.tsx
+++ sfb/static/ts/tptef.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import { Account_tsx, auth, storage, db } from "./component/account";
 
 interface State {
-    room: string; thread:string;
+    room: string; thread: string;
     [key: string]: string;
 }
 
@@ -15,7 +15,7 @@ export class Tptef_tsx extends React.Component<{}, State> {
         docRef.get().then((doc) => {
             if (!doc.exists) {
                 docRef.set({});
-                this.setState({ thread: JSON.stringify({})})
+                this.setState({ thread: JSON.stringify({}) })
             }
             else {
                 this.setState({ thread: JSON.stringify(doc.data()) })
@@ -23,34 +23,26 @@ export class Tptef_tsx extends React.Component<{}, State> {
         });
     }
 
-    thread_table_render(){
+    thread_table_render() {
         const doc_data = JSON.parse(this.state.thread);
-        const thread_record= [];
+        const thread_record = [];
         let keys = Object.keys(doc_data).sort();
         for (var i = 0; i < keys.length; i++) {
-            const thread_data= [];
+            const thread_data = [];
             thread_data.push(<td>{doc_data[keys[i]]["user"]}</td>)
             thread_data.push(<td>{doc_data[keys[i]]["content"]}</td>)
             thread_data.push(<td>{doc_data[keys[i]]["date"]}</td>)
             thread_data.push(<td>{doc_data[keys[i]]["attachment"]}</td>)
             thread_record.push(<tr>{thread_data}</tr>)
         }
-        return(<tbody>{thread_record}</tbody>)
+        return (<tbody>{thread_record}</tbody>)
     }
 
     constructor(props: any) {
         super(props);
         this.state = {
-            room: "main", thread:JSON.stringify({})
+            room: "main", thread: JSON.stringify({})
         };
-        setInterval(() => {
-            if (auth.currentUser) {
-                if (this.state.uid != auth.currentUser.uid) this.setState({ uid: auth.currentUser.uid });
-            }
-            else {
-                if (this.state.uid != "") this.setState({ uid: "" });
-            }
-        }, 100)
     }
 
     render() {
@@ -58,9 +50,9 @@ export class Tptef_tsx extends React.Component<{}, State> {
             <div className="m-2">
                 <h2 style={{ color: "black" }}>Chat_Room</h2>
                 <div className="d-flex justify-content-between">
-                    <input type="text" id="room_name" className="form-control" value={this.state.room} placeholder="Room"
+                    <input className="form-control" id="room_name" type="text" value={this.state.room} placeholder="Room"
                         onChange={(evt) => { this.setState({ room: evt.target.value }) }} />
-                    <button type="button" className="btn btn-success btn-sm ml-auto" onClick={() => { this.load_room() }}>Goto_Room</button>
+                    <button className="btn btn-success btn-sm ml-auto" onClick={() => { this.load_room() }}>Goto_Room</button>
                 </div>
                 <table className="table table-sm bg-light">
                     <thead>
@@ -76,6 +68,18 @@ export class Tptef_tsx extends React.Component<{}, State> {
 
                 <div className="mt-2 p-2" style={{ color: "#AAFEFE", border: "3px double silver", background: "#001111" }}>
                     <div id="submits" style={{ display: "none" }}>
+                        <h5 style={{ color: "white" }}>入力フォーム</h5>
+                        <textarea className="form-control my-1" name="content" placeholder="Content"></textarea>
+                        <div className="my-1 d-flex justify-content-between">
+                            <div className="ml-auto">
+                                <div className="form-inline">
+                                    <input className="form-control form-control-sm mx-1" type="text" value="KARI" />
+                                    <input type="file"/>
+                                    <button className="btn btn-success mx-1">remark</button>
+                                </div>
+                            </div>
+                        </div>
+
                     </div>
                 </div>
             </div >

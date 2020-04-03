6e6ca49f049dde9fabd304fc7231968e338cff41
jSm449g4d <47020182+jSm449g4d@users.noreply.github.com>
2020-04-03 22:17:53 +0900

    sd

--- web_app/wsgi.py
+++ web_app/wsgi.py
@@ -17,7 +17,7 @@ app.config['MAX_CONTENT_LENGTH'] = 100000000
 @app.route("/")
 def indexpage_show():
     wsgi_util.access_counter += 1;
-    return render_template("index.html",
+    return render_template("flask/index.html",
                         STATUS_PYTHON_VERSION=sys.version,
                         STATUS_FLASK_VERSION=flask.__version__,
                         STATUS_ACCESS_COUNT=str(wsgi_util.access_counter),
@@ -27,18 +27,17 @@ def indexpage_show():
 @app.route("/<path:name>.html")
 def html_show(name):
     try:
-        return render_template(os.path.join('./',name,)+'.html'), 200
+        return render_template(os.path.join(name)+'.html'), 200
     except Exception as e:
-        return render_template("error.html", STATUS_ERROR_TEXT=str(e)), 500
+        return render_template("flask/error.html", STATUS_ERROR_TEXT=str(e)), 500
 
 @app.route("/<name>.py")
 def py_show(name):
     try:
         return importlib.import_module(name).show(request)
     except Exception as e:
-        return render_template("error.html", STATUS_ERROR_TEXT=str(e)), 500
+        return render_template("flask/error.html", STATUS_ERROR_TEXT=str(e)), 500
 application = app
-
 
 if __name__ == "__main__":
     app.run()

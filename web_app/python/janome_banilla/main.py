55c100e226e3045ee87263c0a80d3725759aa5fa
jSm449g4d <47020182+jSm449g4d@users.noreply.github.com>
2020-04-06 00:27:29 +0900

    s

---
+++ web_app/python/janome_banilla/main.py
@@ -0,0 +1,60 @@
+import flask
+import os
+import sys
+
+from janome.tokenizer import Tokenizer
+from werkzeug.utils import secure_filename
+
+t = Tokenizer()
+
+def render_template_FaaS(dir, **kwargs):
+    html = ""
+    with open(os.path.join(dir), "r", encoding="utf-8") as f:
+        html = f.read()
+        for kw, arg in kwargs.items():
+            html = html.replace("{{"+kw+"}}", arg)
+    return flask.render_template_string(html)
+
+def show(request):
+    global t
+    ret = ""
+
+    if 'warmup' in request.args:
+        if secure_filename(request.args['warmup']) == "True":
+            return ""
+
+    if request.method == "POST":
+        if 'speech' in request.json:
+            target = request.json['speech'].translate(
+                str.maketrans("\"\'\\/<>%`?;", '””￥_〈〉％”？；'))
+            target = target.translate(str.maketrans("", "", ", "))
+            for token in t.tokenize(target):
+                ret += token.part_of_speech.split(',')[0]+","
+            return ret.strip(',')
+
+        # under constructhion
+        if 'speech2' in request.json:
+            target = request.json['speech2'].translate(
+                str.maketrans("\"\'\\/<>%`?;", '””￥_〈〉％”？；'))
+            target = target.translate(str.maketrans("", "", ", "))
+            for token in t.tokenize(target):
+                ret += token.part_of_speech.split(',')[1]+","
+            return ret.strip(',')
+
+        if 'surface' in request.json:
+            target = request.json['surface'].translate(
+                str.maketrans("\"\'\\/<>%`?;", '””￥_〈〉％”？；'))
+            target = target.translate(str.maketrans("", "", ", "))
+            for token in t.tokenize(target, wakati=True):
+                ret += token+","
+            return ret.strip(',')
+
+        if 'phonetic' in request.json:
+            target = request.json['phonetic'].translate(
+                str.maketrans("\"\'\\/<>%`?;", '””￥_〈〉％”？；'))
+            target = target.translate(str.maketrans("", "", ", "))
+            for token in t.tokenize(target):
+                ret += token.phonetic+","
+            return ret.strip(',')
+
+    return render_template_FaaS(os.path.join(os.path.dirname(__file__),"main.html"), STATUS_PYTHON_VERSION=sys.version, STATUS_FLASK_VERSION=flask.__version__)

b856ecac031f4672bcc29b8a9509038fc909e8ca
jSm449g4d <47020182+jSm449g4d@users.noreply.github.com>
2020-04-10 06:24:20 +0900

    s

--- www/Flask/janome_banilla/main.py
+++ www/Flask/janome_banilla/main.py
@@ -49,5 +49,5 @@ def show(request):
                     ret["phonetic"].append(token.phonetic)
             return json.dumps(ret, ensure_ascii=False)
 
-    render_template_FaaS(os.path.join(
+    return render_template_FaaS(os.path.join(
         os.path.dirname(__file__), "main.html"))

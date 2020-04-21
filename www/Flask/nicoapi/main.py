8e24ea31d14c9358ca67baa0683afb4cd691141d
jSm449g4d <47020182+jSm449g4d@users.noreply.github.com>
2020-04-21 12:52:30 +0900

    f

--- www/Flask/nicoapi/main.py
+++ www/Flask/nicoapi/main.py
@@ -20,7 +20,7 @@ from google.cloud import storage as firestorage
 
 # global variable
 access_counter = 0
-
+restartflag: bool = False
 
 # application
 with open(os.path.join(os.path.dirname(__file__), "config.json"), "r", encoding="utf-8") as fp:
@@ -36,52 +36,55 @@ with open(os.path.join(os.path.dirname(__file__), "config.json"), "r", encoding=
 
 
 def deamon():
-    start_timestamp: int = int(datetime.now().timestamp()*1000)
-    # DB layer
-    docRefs = db.collection('nicoapi').list_documents()
-    for docRef in docRefs:
-        recodes: dict = docRef.get().to_dict()
-        # Document layer
-        # No recode, No Document
-        if len(recodes) < 1:
-            docRef.delete()
-            continue
-        for tsuid, order in recodes.items():
-            # 7days to delete
-            if int(datetime.now().timestamp()*1000) > int(tsuid.split("_")[0])+604800000:
-                recodes[tsuid] = DELETE_FIELD
-                if storage.blob("nicoapi/"+docRef.id + "/"+tsuid + ".zip").exists() == True:
-                    storage.blob("nicoapi/"+docRef.id + "/" +
-                                 tsuid + ".zip").delete()
+    global restartflag
+    while restartflag == True:
+        restartflag = False
+        start_timestamp: int = int(datetime.now().timestamp()*1000)
+        # DB layer
+        docRefs = db.collection('nicoapi').list_documents()
+        for docRef in docRefs:
+            recodes: dict = docRef.get().to_dict()
+            # Document layer
+            # No recode, No Document
+            if len(recodes) < 1:
+                docRef.delete()
                 continue
-            # downloaded data is not exist on GCS → delete
-            if recodes[tsuid]["status"] == "processed":
-                if storage.blob("nicoapi/"+docRef.id + "/"+tsuid + ".zip").exists() == False:
+            for tsuid, order in recodes.items():
+                # 7days to delete
+                if int(datetime.now().timestamp()*1000) > int(tsuid.split("_")[0])+604800000:
                     recodes[tsuid] = DELETE_FIELD
-                continue
-            with io.BytesIO() as inmemory_zip:
-                # set https UserAgent
-                https = urllib3.PoolManager(cert_reqs='CERT_REQUIRED', ca_certs=certifi.where(
-                ), headers={"User-Agent": order["User-Agent"]})
-                # start Crawl
-                for url in order["request_urls"]:
-                    print(hashlib.md5(url.encode('utf-8')).hexdigest())
-                    time.sleep(3)
-                    try:
-                        resp_json = https.request('GET', parse.quote(
-                            url, safe="=&-?:/%")).data.decode('utf-8')
-                        with zipfile.ZipFile(inmemory_zip, 'a', compression=zipfile.ZIP_DEFLATED) as zf:
-                            zf.writestr(hashlib.md5(url.encode(
-                                'utf-8')).hexdigest(), json.dumps(resp_json, ensure_ascii=False))
-                        storage.blob("nicoapi/"+docRef.id + "/"+tsuid +
-                                     ".zip").upload_from_string(inmemory_zip.getvalue())
-                    except:
-                        pass
-            recodes[tsuid]["status"] = "processed"
-        docRef.set(recodes, merge=True)
-    # prevent high freq restart
-    while int(datetime.now().timestamp()*1000) < start_timestamp+3000:
-        time.sleep(1)
+                    if storage.blob("nicoapi/"+docRef.id + "/"+tsuid + ".zip").exists() == True:
+                        storage.blob("nicoapi/"+docRef.id + "/" +
+                                     tsuid + ".zip").delete()
+                    continue
+                # downloaded data is not exist on GCS → delete
+                if recodes[tsuid]["status"] == "processed":
+                    if storage.blob("nicoapi/"+docRef.id + "/"+tsuid + ".zip").exists() == False:
+                        recodes[tsuid] = DELETE_FIELD
+                    continue
+                with io.BytesIO() as inmemory_zip:
+                    # set https UserAgent
+                    https = urllib3.PoolManager(cert_reqs='CERT_REQUIRED', ca_certs=certifi.where(
+                    ), headers={"User-Agent": order["User-Agent"]})
+                    # start Crawl
+                    for url in order["request_urls"]:
+                        print(hashlib.md5(url.encode('utf-8')).hexdigest())
+                        time.sleep(3)
+                        try:
+                            resp_json = https.request('GET', parse.quote(
+                                url, safe="=&-?:/%")).data.decode('utf-8')
+                            with zipfile.ZipFile(inmemory_zip, 'a', compression=zipfile.ZIP_DEFLATED) as zf:
+                                zf.writestr(hashlib.md5(url.encode(
+                                    'utf-8')).hexdigest(), json.dumps(resp_json, ensure_ascii=False))
+                            storage.blob("nicoapi/"+docRef.id + "/"+tsuid +
+                                         ".zip").upload_from_string(inmemory_zip.getvalue())
+                        except:
+                            pass
+                recodes[tsuid]["status"] = "processed"
+            docRef.set(recodes, merge=True)
+        # prevent high freq restart
+        while int(datetime.now().timestamp()*1000) < start_timestamp+3000:
+            time.sleep(1)
 
 
 thread_d = threading.Thread(name='nicoapi_d', target=deamon)
@@ -96,11 +99,15 @@ def show(request):
         "Thread": "Running", }
 
     # body
-    global thread_d
+    global thread_d, restartflag
     if thread_d.is_alive() == False:
+        restartflag = True
         thread_d = threading.Thread(name='nicoapi_d', target=deamon)
         thread_d.start()
         status_dict["thread"] = "start"
+    else:
+        restartflag = True
+
     if request.method == "POST":
         return json.dumps(status_dict, ensure_ascii=False), 200
 

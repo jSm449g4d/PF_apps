a48159c0414dcb3e1b3042a2ec42892c5a79ab32
jSm449g4d <47020182+jSm449g4d@users.noreply.github.com>
2020-04-21 17:47:19 +0900

    h

--- www/integration.sh
+++
@@ -1,9 +0,0 @@
-# Integration test 1/2
-waitress-serve --host=127.0.0.1 --port=8080 wsgi:app &
-result_integration=$(python3 integration.py)
-pkill -f waitress-serve
-if [ $result_integration = "ok" ]; then
-    echo PASS:Integration Test;exit 0
-else
-    echo REJECT:$result_integration;exit 1
-fi

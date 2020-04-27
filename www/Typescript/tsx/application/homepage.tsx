550ce8a573fc1fcaec467393349e5c4428f71ceb
jSm449g4d <47020182+jSm449g4d@users.noreply.github.com>
2020-04-27 15:50:29 +0900

    carouselTest

--- www/Typescript/tsx/application/homepage.tsx
+++ www/Typescript/tsx/application/homepage.tsx
@@ -38,7 +38,7 @@ export const AppMain = () => {
         const _topicBlocks = [tolicBlock("1"), tolicBlock("2"), tolicBlock("3"), tolicBlock("4"), tolicBlock("5")]
         if (topicBlockIterator > _topicBlocks.length - 3) { setTopicBlockIterator(0); };
         if (topicBlockIterator < 0) { setTopicBlockIterator(_topicBlocks.length - 3); };
-        return (
+        /*
             <div className="d-flex justify-content-center align-content-center">
                 <i className="fas fa-caret-left fa-4x mr-1" style={{ color: "lightcyan" }}
                     onClick={() => { setTopicBlockIterator(topicBlockIterator - 1) }}></i>
@@ -47,6 +47,36 @@ export const AppMain = () => {
                 {_topicBlocks[topicBlockIterator + 2]}
                 <i className="fas fa-caret-right fa-4x mr-1" style={{ color: "lightcyan" }}
                     onClick={() => { setTopicBlockIterator(topicBlockIterator + 1) }}></i>
+            </div>*/
+        return (
+            <div id="topicBlockCarousel" className="carousel slide" data-ride="carousel">
+                <ol className="carousel-indicators">
+                    <li className="active" data-target="#topicBlockCarousel" data-slide-to="0"></li>
+                    <li data-target="#topicBlockCarousel" data-slide-to="1"></li>
+                    <li data-target="#topicBlockCarousel" data-slide-to="2"></li>
+                </ol>
+                <div className="carousel-inner">
+                    <div className="carousel-item active">
+                        <img className="d-block w-100" src="/static/img/aircraft-2795557_1280.jpg" alt="First slide">
+                        </img>
+                    </div>
+                    <div className="carousel-item">
+                        <img className="d-block w-100" src="/static/img/aircraft-2795557_1280.jpg" alt="Second slide">
+                        </img>
+                    </div>
+                    <div className="carousel-item">
+                        <img className="d-block w-100" src="/static/img/aircraft-2795557_1280.jpg" alt="Third slide">
+                        </img>
+                    </div>
+                    <a className="carousel-control-prev" href="#topicBlockCarousel" role="button" data-slide="prev">
+                        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span className="sr-only">Previous</span>
+                    </a>
+                    <a className="carousel-control-next" href="#topicBlockCarousel" role="button" data-slide="next">
+                        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span className="sr-only">Next</span>
+                    </a>
+                </div>
             </div>
         )
     }
